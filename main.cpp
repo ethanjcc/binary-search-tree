18078c1b0c5cfca6babc8b5948849e16ae350d11
U-BSD\453370 <453370@SHS-HYBF6X3.beaverton.k12.or.us>
2026-03-31 13:31:49 -0700

    third commit

--- main.cpp
+++ main.cpp
@@ -5,19 +5,36 @@
 using namespace std;
 
 int main(){
+  bst tree;
   char input[50];
-  cout << "what do you want" << endl;
-  cin >> input;
-  if (strcmp(input, "insert") == 0) {
-    insert(Node* root, int value);
-  }
-  else if (strcmp(input, "remove") == 0) {
-
-  }
-  else if (strcmp(input, "search") == 0) {
-    search(Node* root, int value) const;
-  }
-  else if (strcmp(input, "tree") == 0) {
-
+  while (true) {
+    cout << "what do you want" << endl;
+    cin >> input;
+    if (strcmp(input, "insert") == 0) {
+      int value;
+      cin >> value;
+      tree.insert(value);
+    }
+    else if (strcmp(input, "remove") == 0) {
+      int value;
+      cin >> value;
+      tree.remove(value);
+    }
+    else if (strcmp(input, "search") == 0) {
+      int value;
+      cin >> value;
+      if (tree.search(value)) {
+	cout << "found it" << endl;
+      }
+      else {
+	cout << "didn't find it" << endl;
+      }
+    }
+    else if (strcmp(input, "tree") == 0) {
+      tree.tree();
+    }
+    else if (strcmp(input, "quit") == 0) {
+      break;
+    }
   }
 }
