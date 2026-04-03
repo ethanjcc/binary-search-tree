12ce361f7b68e481d455319a18faff7d85a19787
ethanjcc <453370@bsd48.org>
2026-04-03 16:17:03 -0700

    Update bst.cpp
    didnt push this file for some reason

--- bst.cpp
+++ bst.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-Node::Node(char value) {
+Node::Node(int value) {
   data = value;
   left = nullptr;
   right = nullptr;
@@ -20,6 +20,12 @@ bst::~bst() {
 
 }
 
+//update root
+void bst::insert(int value) {
+  root = insert(root, value);
+}
+
+//code for inserting a number into the tree
 Node* bst::insert(Node* root, int value) {
   if (root == nullptr) {
     return new Node(value);
@@ -28,11 +34,18 @@ Node* bst::insert(Node* root, int value) {
     root->left = insert(root->left, value);
   }
   else if (value > root->data) {
-    root->right = insert(root->right, value)
+    root->right = insert(root->right, value);
   }
   return root;
 }
 
+//update function
+//copilot used to understand need for const
+bool bst::search(int value) const {
+  return search(root, value);
+}
+
+//code for finding a number in the tree
 bool bst::search(Node* root, int value) const {
   if (root == nullptr) {
     return false;
@@ -43,9 +56,77 @@ bool bst::search(Node* root, int value) const {
   }
   //found
   if (value < root->data) {
-    return true;
+    return search(root->left, value);
   }
   else {
     return search(root->right, value);
   }
+}
+
+//update root
+void bst::remove(int value) {
+  root = remove(root, value);
+}
+
+//code for removing a number in the tree
+//help from copilot used
+Node* bst::remove(Node* root, int value) {
+  //no number found
+  if (root == nullptr) {
+    return nullptr;
+  }
+  if (value < root->data) {
+    root->left = remove(root->left, value);
+  }
+  else if (value > root->data) {
+    root->right = remove(root->right, value);
+  }
+  else {
+    //no child
+    if (root->left == nullptr && root->right == nullptr) {
+      delete root;
+      return nullptr;
+    }
+    //one child on the right
+    if (root->left == nullptr) {
+      Node* temp = root->right;
+      delete root;
+      return temp;
+    }
+    //one child on the left
+    if (root->right == nullptr) {
+      Node* temp = root->left;
+      delete root;
+      return temp;
+    }
+    //two children. Has to find a node to inherit its spot
+    Node* inherit = root->right;
+    while (inherit->left != nullptr) {
+      inherit = inherit->left;
+    }
+    root->data = inherit->data;
+    //delete the node
+    root->right = remove(root->right, inherit->data);
+  }
+  return root;
+}
+
+//public tree print
+void bst::tree() const {
+  tree(root, 0);
+}
+
+//tree printing code
+void bst::tree(Node* root, int depth) const {
+  if (root == nullptr) {
+    return;
+  }
+  //right subtree
+  tree(root->right, depth + 1);
+  //indent if needed
+  for (int i = 0; i < depth; i++){
+    cout << "\t";
+  }
+  cout << root->data << endl;
+  tree(root->left, depth + 1);
 }
